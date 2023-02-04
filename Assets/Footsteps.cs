06ba00f10ce57b4fde7f44f58648b62ea5115402
pootyboop <elliotgmann@gmail.com>
2023-02-04 15:24:58 +0000

    step strength

--- Assets/Footsteps.cs
+++ Assets/Footsteps.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    void Start()
+    public bool isRight = false;
+    Vector3 lastPosition, currentPosition;
+    const float maxStrength = 6.0f;
+
+    private void FixedUpdate()
     {
-        print("start");
+        lastPosition = currentPosition;
+        currentPosition = transform.position;
     }
 
 
@@ -14,11 +19,20 @@ public class Footsteps : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
 
-        print("collision!");
+        //print("collision!");
 
         if (collision.gameObject.CompareTag("Ground"))
         {
-            print("collision detect: " + collision.contacts[0]);
+            print(StepStrength(lastPosition, currentPosition));
         }
+    }
+
+
+
+    float StepStrength(Vector3 pos1, Vector3 pos2)
+    {
+        float strength = Vector3.Distance(pos1, pos2) * 100.0f;
+        
+        return Mathf.Clamp(strength / maxStrength, 0.0f, 1.0f);
     }
 }
