459c304ae4b615fb59bbc1c7ec06f14f711f65b6
pootyboop <elliotgmann@gmail.com>
2023-02-05 16:10:59 +0000

    socks

---
+++ Assets/Scripts/Shoe.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoe : MonoBehaviour
+{
+    public Footsteps footL, footR;
+    public bool isRight = false;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(gameObject + " overlapped " + other.gameObject);
+        if (/*other.gameObject.GetInstanceID() == footL.GetInstanceID() && */!isRight && !footL.hasShoe)
+        {
+            print("left success");
+            footL.WearShoe();
+            Destroy(gameObject);
+        }
+
+        else if (/*GameObject.ReferenceEquals(other.gameObject, footR) && */isRight && !footR.hasShoe)
+        {
+            print("right success");
+            footR.WearShoe();
+            Destroy(gameObject);
+        }
+    }
+}
